2ec44fe4199bfcb80406c82abadae8333a2e6e32
nayelisilva06 <nayelizunigasilva@gmail.com>
2025-06-15 18:25:12 -0600

    Remove unused import from event_service.dart

--- lib/features/events/services/event_service.dart
+++ lib/features/events/services/event_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'package:http/http.dart' as http;
 import 'package:notificador/features/events/models/create_event_model.dart';
 import 'package:notificador/features/events/models/update_event_model.dart';
